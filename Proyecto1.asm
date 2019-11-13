17f5eb8bc99763ace214cca594adae7a69cab67d
JOSE ANDRES <achaconm2001@gmail.com>
2019-11-12 20:17:34 -0600

    Prueba 1

--- Proyecto1.asm
+++ Proyecto1.asm
@@ -1,16 +1,19 @@
 %include "Helpers.asm"
 
 SECTION .data
-    cantLetras: db 26
-    frecuencias: db 12.53, 1.42, 4.68, 5.86, 13.68, 0.69, 1.01, 0.70, 6.25, 0.44, 0.02, 4.97, 3.15, 6.71, 8.68, 2.51, 0.88, 6.87, 7.98, 4.63, 3.93, 0.90, 0.01, 0.22, 0.90, 0.52
+    
+    letras equ 26
     node equ 29
     nodeForest equ 12
-    lastNode: db 0
+    
+    cantLetras: dd letras
+    lastNode: dd 0
+    frecuencias: dd 12.53, 1.42, 4.68, 5.86, 13.68, 0.69, 1.01, 0.70, 6.25, 0.44, 0.02, 4.97, 3.15, 6.71, 8.68, 2.51, 0.88, 6.87, 7.98, 4.63, 3.93, 0.90, 0.01, 0.22, 0.90, 0.52
 
 
 SECTION .bss
-    forest: resb 26 * nodeForest
-    tree: resb 26 * node * 2
+    forest: resb letras * nodeForest
+    tree: resb letras * node * 2
 
 SECTION .text
     global _start
